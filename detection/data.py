fedd423112710b1426c9082a8435d785379cd8b9
Jacob Schäfer <js.jacobschaefer@gmail.com>
2022-07-16 16:05:56 +0200

    use dict instead of list for samples

--- detection/data.py
+++ detection/data.py
@@ -17,11 +17,13 @@ import os
 import pandas as pd
 
 ds_path = 'datasets/cancer'
-#https://lindevs.com/download-dataset-from-kaggle-using-api-and-python/
-#https://www.kaggle.com/docs/api
-#https://github.com/Kaggle/kaggle-api/blob/master/kaggle/api/kaggle_api_extended.py
 
-def unzip_competition_files(competition,path):
+
+# https://lindevs.com/download-dataset-from-kaggle-using-api-and-python/
+# https://www.kaggle.com/docs/api
+# https://github.com/Kaggle/kaggle-api/blob/master/kaggle/api/kaggle_api_extended.py
+
+def unzip_competition_files(competition, path):
     outfile = os.path.join(path, competition + '.zip')
     try:
         with zipfile.ZipFile(outfile) as z:
@@ -31,67 +33,72 @@ def unzip_competition_files(competition,path):
             'Bad zip file, please report on '
             'www.github.com/kaggle/kaggle-api', e)
     try:
-            os.remove(outfile)
+        os.remove(outfile)
     except OSError as e:
         print('Could not delete zip file, got %s' % e)
 
-def load_competition_from_kaggle(competition,path):
+
+def load_competition_from_kaggle(competition, path):
     from kaggle.api.kaggle_api_extended import KaggleApi
     api = KaggleApi()
     api.authenticate()
     api.competition_download_files(competition, path, quiet=False)
-    unzip_competition_files(competition,path)
+    unzip_competition_files(competition, path)
+
 
 def load_cancer_ds():
     competition = 'histopathologic-cancer-detection'
     path = ds_path
 
     if not os.path.exists(path):
-        load_competition_from_kaggle(competition,path)
+        load_competition_from_kaggle(competition, path)
 
-#https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
+
+# https://pytorch.org/tutorials/beginner/data_loading_tutorial.html
 class CancerDataset(Dataset):
-    def __init__(self,path,csvfile,transform = None):
+    def __init__(self, path, csvfile, transform=None):
         self.path = path
         self.labels_frame = pd.read_csv(csvfile)
         self.transform = transform
-    
+
     def __len__(self):
         return len(self.labels_frame)
-    
-    def __getitem__(self,idx):
+
+    def __getitem__(self, idx):
         if torch.is_tensor(idx):
             idx = idx.tolist()
 
         img_name = os.path.join(self.path,
-                                self.labels_frame.iloc[idx, 0]+".tif")
+                                self.labels_frame.iloc[idx, 0] + ".tif")
         image = io.imread(img_name)
         label = self.labels_frame.iloc[idx, 1]
 
         if self.transform:
             image = self.transform(image)
 
-
-        sample = [image,label]
+        sample = {'image': image, 'label': label}
 
         return sample
 
 
 def get_ds():
     load_cancer_ds()
-    transforms = torchvision.transforms.Compose([torchvision.transforms.ToTensor(), torchvision.transforms.Resize([96,96])])
-    ds = CancerDataset(os.path.join(ds_path, "train"),os.path.join(ds_path, "train_labels.csv"), transforms)
+    transforms = torchvision.transforms.Compose(
+        [torchvision.transforms.ToTensor(), torchvision.transforms.Resize([96, 96])])
+    ds = CancerDataset(os.path.join(ds_path, "train"), os.path.join(ds_path, "train_labels.csv"), transforms)
     return ds
 
-def get_dl(batch_size,num_workers,pin_memory=True):
+
+def get_dl(batch_size, num_workers, pin_memory=True):
     ds = get_ds()
     img_shape = ds[0][0].shape
-    dl = DataLoader(ds, batch_size=batch_size,shuffle=True, num_workers=num_workers,pin_memory=pin_memory)
+    dl = DataLoader(ds, batch_size=batch_size, shuffle=True, num_workers=num_workers, pin_memory=pin_memory)
     return dl, img_shape
+
 
 if __name__ == "__main__":
     ds = get_ds()
     sample = ds.__getitem__(0)
-    print(sample["label"])
-    plt.imshow(torchvision.transforms.ToPILImage()(sample["image"]))
+    print(sample['label'])
+    plt.imshow(torchvision.transforms.ToPILImage()(sample['image']))
     plt.show()
--- detection/trainer.py
+++ detection/trainer.py
@@ -21,17 +21,20 @@ import model
 import data
 import wandb
 
-#https://pytorch.org/tutorials/beginner/basics/optimization_tutorial.html
 
-def get_model(img_shape,normalize):
-    return model.Classifier(img_shape,normalize)
+# https://pytorch.org/tutorials/beginner/basics/optimization_tutorial.html
 
-def train(model,dataloader,device,batch_size=64,learning_rate=1e-3,epochs=5):
+def get_model(img_shape, normalize):
+    return model.Classifier(img_shape, normalize)
+
+
+def train(model, dataloader, device, batch_size=64, learning_rate=1e-3, epochs=5):
     loss_fn = nn.BCEWithLogitsLoss()
     optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
-    train_loop(model,dataloader,loss_fn,optimizer,device,epochs)
+    train_loop(model, dataloader, loss_fn, optimizer, device, epochs)
 
-def train_loop(model,dataloader,loss_fn,optimizer,device,epochs):
+
+def train_loop(model, dataloader, loss_fn, optimizer, device, epochs):
     model = model.to(device)
     size = len(dataloader.dataset)
     for epoch in range(epochs):
@@ -39,7 +42,7 @@ def train_loop(model,dataloader,loss_fn,optimizer,device,epochs):
             # Compute prediction and loss
             X = X.to(device)
             y = y.to(device)
-            y = y.view(-1,1).to(torch.float)
+            y = y.view(-1, 1).to(torch.float)
             pred = model(X)
             loss = loss_fn(pred, y)
 
@@ -48,38 +51,40 @@ def train_loop(model,dataloader,loss_fn,optimizer,device,epochs):
             loss.backward()
             optimizer.step()
 
-            print(str(batch),end='\r')
+            print(str(batch), end='\r')
             if batch % 100 == 0:
                 loss, current = loss.item(), batch * len(X)
                 print(f"loss: {loss:>7f}  [{current:>5d}/{size:>5d}]")
-                wandb.log({"loss":loss})
-    
+                wandb.log({"loss": loss})
 
-def run_classifier(trainer_config,model_config):
+
+def run_classifier(trainer_config, model_config):
     wandb.init(project=trainer_config["project"], entity="histo-cancer-detection")
     wandb.config = model_config
-    dataloader,img_shape = data.get_dl(batch_size=model_config["batch_size"],num_workers=model_config["num_workers"])
-    model = get_model(img_shape,True)
+    dataloader, img_shape = data.get_dl(batch_size=model_config["batch_size"], num_workers=model_config["num_workers"])
+    model = get_model(img_shape, True)
     wandb.watch(model)
     print(trainer_config["device"])
-    train (model,dataloader, trainer_config["device"],batch_size=model_config["batch_size"],learning_rate=model_config["learning_rate"],epochs=model_config["max_epochs"])
+    train(model, dataloader, trainer_config["device"], batch_size=model_config["batch_size"],
+          learning_rate=model_config["learning_rate"], epochs=model_config["max_epochs"])
+
 
 # decreases logging for better performance! mostly relevant for small dsets
 PERFORMANCE_MODE = False
 PRJ = "histo_cancer"
 
 MODEL_CONFIG = dict(
-    batch_size = 64,
-    num_workers = 4,
-    learning_rate = 0.01,
+    batch_size=64,
+    num_workers=4,
+    learning_rate=0.01,
     max_epochs=100,
 )
 
 GPUS = 1
 TRAINER_CONFIG = dict(
-    project = PRJ,
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
+    project=PRJ,
+    device=torch.device("cuda:0" if torch.cuda.is_available() else "cpu"),
 )
 
 if __name__ == "__main__":
-    run_classifier(TRAINER_CONFIG,MODEL_CONFIG)
+    run_classifier(TRAINER_CONFIG, MODEL_CONFIG)
