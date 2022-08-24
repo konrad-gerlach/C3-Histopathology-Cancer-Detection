ca31fd35d43cc07830fec1f1f4eec21b4c2a4e6a
Till Wenke <till.wenke@gmx.net>
2022-08-24 22:06:08 +0300

    setting the gradient zero after backprop does not solve it

--- detection/visualize_saliency_maps.py
+++ detection/visualize_saliency_maps.py
@@ -41,18 +41,20 @@ def setup(grayscale, good_model):
 
 
 def show_saliencies(images):
-    fig, ax = plt.subplots(5, len(images))
-    for i, image in enumerate(images):
+    fig, ax = plt.subplots(2, len(images))
+    for i, image_and_gradient in enumerate(images):
+        image = image_and_gradient[0]
+        gradient = image_and_gradient[1]
         # red = larger absolut gradient in one of the 3 channels
         # ... just shows certainty (not if for or against cancer?)
-        sal_abs, _ = torch.max(image.grad.data.abs(), dim=1)
+        sal_abs, _ = torch.max(gradient.abs(), dim=1)
         sal_abs = sal_abs.reshape(96, 96)
 
         # red = one of the channels has super high gradient
         # vs black = one of the channels has super low gradient
         # ... most certain channel wins
-        sal_max, _ = torch.max(image.grad.data, dim=1)
-        sal_min, _ = torch.min(image.grad.data, dim=1)
+        sal_max, _ = torch.max(gradient, dim=1)
+        sal_min, _ = torch.min(gradient, dim=1)
 
         geq = sal_max.abs() >= sal_min.abs()
         geq = geq.type(torch.int)
@@ -70,15 +72,12 @@ def show_saliencies(images):
         ax[0, i].imshow(img.cpu().detach().numpy().transpose(1, 2, 0))
         ax[0, i].axis('off')
 
-        ax[1, i].imshow(sal_abs.cpu(), cmap='hot')
+        ax[1, i].imshow(sal_abs.cpu(), cmap='inferno')
         ax[1, i].axis('off')
-        ax[2, i].imshow(sal_abs.cpu(), cmap='inferno')
-        ax[2, i].axis('off')
 
-        ax[3, i].imshow(saliency.cpu(), cmap='hot')
-        ax[3, i].axis('off')
-        ax[4, i].imshow(saliency.cpu(), cmap='inferno')
-        ax[4, i].axis('off')
+        inferno = plt.get_cmap('inferno')
+        wandb.log({"input" : wandb.Image(img.cpu().detach().numpy().transpose(1, 2, 0))})
+        wandb.log({"full" : wandb.Image(inferno(sal_abs))})
 
     wandb.log({"Cancer images with saliency maps": plt})
 
@@ -116,40 +115,44 @@ def cancer_regions(sal_abs, image):
     plt.show()
 
 
-def collect_images_with_gradient(grayscale, good_model, num_images, images):
+def collect_images_with_gradient(grayscale, good_model, num_images, images_with_gradient):
     image_data, device, model = setup(grayscale, good_model)
-    num_images = len(images) + num_images
-
-    for batch, (X, y) in enumerate(image_data):
+    num_images = len(images_with_gradient) + num_images
+    
+    for batch, (image, y) in enumerate(image_data):
+        
         if y:
-            X = X.to(device)
-            X.requires_grad_()
-            images.append(X)
-        if len(images) >= num_images:
+            image = image.to(device)
+            print(image.grad == None)
+            image.requires_grad_()   
+
+            # Retrieve output from the image
+            output = model(image)
+            # Do backpropagation to get the derivative of the output based on the image
+            output.backward()
+            #print(image.grad.data)
+            images_with_gradient.append([image, image.grad.data])
+            #image.grad = None
+           
+                
+        if len(images_with_gradient) >= num_images:
             break
+    
 
-    for image in images:
-        # Retrieve output from the image
-        output = model(image)
-        # Catch the output
-        output_idx = output.argmax()
-        output_max = output[0, output_idx]
-        # Do backpropagation to get the derivative of the output based on the image
-        output_max.backward()
-
-    return images
+    return images_with_gradient
 
 
 def saliency_visualizer():
     # configure here
 
     num_images = 3
-    images = []
-    images = collect_images_with_gradient(False, True, num_images, images)
-    images = collect_images_with_gradient(False, False, num_images, images)
-    images = collect_images_with_gradient(True, True, num_images, images)
+    images_with_gradient = []    
+    images_with_gradient = collect_images_with_gradient(False, True, num_images, images_with_gradient)
+    images_with_gradient = collect_images_with_gradient(False, False, num_images, images_with_gradient)
+    images_with_gradient = collect_images_with_gradient(True, True, num_images, images_with_gradient)
+    
 
-    show_saliencies(images)
+    show_saliencies(images_with_gradient)
 
 
 if __name__ == "__main__":
